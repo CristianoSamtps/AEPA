98a2c2351829834827d50432fe8e4a9a2c5d5922
Gonçalo Oliveira <2221008@my.ipleiria.pt>
2023-11-15 17:25:36 +0000

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
       <div class="row">
         <div class="col-md-6">
           <h1>Já conheces a missão da nossa associação AEPA?</h1>
-          <h5>Com mais de 14 mil apoiantes já reconstruímos centenas de hectares! Tudo graças à nossa maravilhosa equipa.
+          <h5>Com mais de 12 mil apoiantes já reconstruímos centenas de hectares! Tudo graças à nossa maravilhosa equipa.
           </h5>
           <a><button class="btn hero-btn1">Associação</button></a>
           <a><button class="btn hero-btn2">Doações</button></a>
