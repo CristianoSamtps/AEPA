c7e4d37a0a50686f60d311c72480228fa29c30fa
Cristiano <cristiano_leiria_epl@outlook.com>
2023-11-15 18:02:33 +0000

    Auto stash before merge of "main" and "origin/main"
    Navbar update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,15 +37,18 @@
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
-    <svg width="965" height="192" viewBox="0 0 965 192" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M127.464 142.481C-18.8682 100.644 0.893704 0 0.893704 0H938.5C938.5 0 965 37 965 82.5C965 128 935 176.5 865.5 187.5C823.076 194.215 772.382 192.615 731.058 179.59C659.506 157.037 592.19 144.09 522.991 157.037C369.833 185.693 273.796 184.317 127.464 142.481Z" fill="url(#paint0_linear_213_3)"/>
-    <defs>
-    <linearGradient id="paint0_linear_213_3" x1="458.5" y1="0" x2="458.5" y2="545.452" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#125D35"/>
-    <stop offset="0.244792" stop-color="#1B9861"/>
-    </linearGradient>
-    </defs>
-    </svg>
+<svg width="997" height="155" viewBox="0 0 997 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M119.832 113.279C-29.3934 71.3393 2.95979 0 2.95979 0H994.286C994.286 0 1000.39 45.1112 994.286 80.6988C988.18 116.287 961.653 149.869 893.329 154.38C825.004 158.892 759.739 137.86 704.671 120.798C631.704 98.1888 580.461 124.359 509.895 137.338C353.709 166.065 269.058 155.219 119.832 113.279Z" fill="url(#paint0_linear_214_4)"/>
+<defs>
+<linearGradient id="paint0_linear_214_4" x1="469.614" y1="5.13046e-08" x2="469.614" y2="546.799" gradientUnits="userSpaceOnUse">
+<stop stop-color="#125D35"/>
+<stop offset="0.244792" stop-color="#1B9861"/>
+</linearGradient>
+</defs>
+</svg>
+
+
+
 
 
 
