5686282523bc02798b930f6d9a9155775f5e883f
Giovanna Salgueiro <giovanna18salguero@gmail.com>
2023-11-17 10:31:05 +0000

    Giovanna
    Atualizei top donates e adicionei styleG

--- resources/views/topDonates.blade.php
+++ resources/views/topDonates.blade.php
@@ -2,19 +2,28 @@
 
 @section('title', 'AEPA')
 
+
+@section ('styles')
+<link href="{{ asset ('/css/styleG.css') }}" rel="stylesheet">
+@endsection
+
 @section('main')
 
 <main id="main"> 
-  <section class="top-donates" >
+  <section class="top-donates">
   <div class="titulo">
         <h2>Top donates</h2>
     </div>
     <div class="filtros">
         <div class="input-group mb-3" id="box">
-        <input type="text" class="form-control" placeholder="Pesquisar" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <input type="text" class="form-control" placeholder="Pesquisar" aria-label="Recipient's username" aria-describedby="button-addon2">
         </div>
+        <span class="switch">
+            <input type="checkbox" name="" id="switcher">
+            <label for="switcher"></label>
+        </span>
     </div>
-
+    
     <div class="donates">
         <div class="card">
             <img src="img/ana.jpg" alt="" class="profile-image">
