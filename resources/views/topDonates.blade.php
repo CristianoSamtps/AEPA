cea87429fdbebcdedef28041c16e367f9eab42da
Giovanna Salgueiro <giovanna18salguero@gmail.com>
2023-11-17 11:36:04 +0000

    Responsivo
    Coloquei o topDonates responsivo

--- resources/views/topDonates.blade.php
+++ resources/views/topDonates.blade.php
@@ -9,43 +9,46 @@
 
 @section('main')
 
-<main id="main"> 
+<main id="main">
   <section class="top-donates">
   <div class="titulo">
         <h2>Top donates</h2>
     </div>
     <div class="filtros">
-        <div class="input-group mb-3" id="box">
-            <input type="text" class="form-control" placeholder="Pesquisar" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="search-box">
+            <input type="text" placeholder="Pesquisar...">
+            <a href="">
+                <img src="{{ asset('img/lupa.svg')}}" alt="">
+            </a>
         </div>
         <span class="switch">
             <input type="checkbox" name="" id="switcher">
             <label for="switcher"></label>
         </span>
     </div>
-    
+
     <div class="donates">
         <div class="card">
-            <img src="img/ana.jpg" alt="" class="profile-image">
+            <img src="{{ asset('img/ana.jpg')}}" alt="" class="profile-image">
             <h2 class="name">Joana Bastos</h2>
             <button class="btn">1361€</button>
         </div>
         <div class="card">
-            <img src="img/ana.jpg" alt="" class="profile-image">
+            <img src="{{ asset('img/ana.jpg')}}" alt="" class="profile-image">
             <h2 class="name">Joel Loureiro</h2>
             <button class="btn">1361€</button>
         </div>
         <div class="card">
-            <img src="img/ana.jpg" alt="" class="profile-image">
+            <img src="{{ asset('img/ana.jpg')}}" alt="" class="profile-image">
             <h2 class="name">Joel Loureiro</h2>
             <button class="btn">1361€</button>
         </div>
         <div class="card">
-            <img src="img/ana.jpg" alt="" class="profile-image">
+            <img src="{{ asset('img/ana.jpg')}}" alt="" class="profile-image">
             <h2 class="name">Joel Loureiro</h2>
             <button class="btn">1361€</button>
         </div
-    </div>  
+    </div>
 
 </section>
 
