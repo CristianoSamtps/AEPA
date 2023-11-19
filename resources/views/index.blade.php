7aa7fedfad18082dc13925bff35c0325f5fbb2ac
CristianoSantos <cristiano_leiria_epl@hotmail.com>
2023-11-19 20:04:14 +0000

    Auto stash before merge of "main" and "origin/main"
    Versão index responsiva

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,13 +41,13 @@
     </div>
     <div class="row">
       <div class="col-md-12 info-row mid-section-info">
-        <div>
+        <div id="volinfo">
           <h3>Faça um dia em família</h3>
-          <p>apoie esta causa e crie momentos para relembrar</p>
+          <p>Apoie esta causa e crie momentos para relembrar.</p>
         </div>
         <div id="sectionbtn">
-          <a><button class="btn section-btn1">Associação</button></a>
-          <a><button class="btn section-btn2">Doações</button></a>
+          <a><button class="btn section-btn1">Voluntariado</button></a>
+          <a><button class="btn section-btn2">Eventos</button></a>
         </div>
       </div>
     </div>
@@ -65,10 +65,9 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 hidden">
-          <p class="sectionIndicator">Projetos</p>
+          <p class="sectionIndicator">Projetos em destaque</p>
           <h1>Estamos a construir um mundo melhor, com um futuro sustentável e duradouro.</h1>
           <a class="link-light" href="{{route('projects')}}"><button class="btn green-btn1">Ver mais Projetos</button></a>
-
         </div>
         <div class="col-md-6 hidden2">
           <div class="d-flex projects carousel-track" id="carouselTrack">
@@ -82,7 +81,6 @@
               <img src="{{asset ('img/projLeiria.png')}}" alt="projLeiria">
               <p class="projarrow">Saber mais -></p>
             </div>
-
             <div class="item projimg">
               <div class="item-info">
                 <div class="item-info-container">
@@ -95,7 +93,6 @@
                 <p class="projarrow">Saber mais -></p>
               </div>
             </div>
-
             <div class="item projimg">
               <div class="item-info">
                 <div class="item-info-container">
@@ -119,34 +116,35 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12 text-center">
+        <p class="">Missão da AEPA</p>
         <h1>Salvamos o planeta <span class="spanenfase">juntos</span></h1>
         <h5>Com mais de 12 mil apoiantes já reconstruímos centenas de hectares! Tudo graças à nossa maravilhosa equipa.
         </h5>
       </div>
-      <div class="col-md-12 d-flex justify-content-center">
+      <div class="col-md-12 d-flex justify-content-center m-section">
         <div class="m-5">
           <img class="volillustration" src="{{asset ('img/limpeza.svg')}}" alt="ilustração de limpeza">
           <h3 class="text-center font-weight-bold">Limpeza</h3>
-          <p class="text-center">Fazer a manutenção das florestas e reciclar os materiais recolhidos</p>
+          <p class="text-center">Fazer limpeza das florestas e reciclar materiais</p>
         </div>
         <div class="m-5">
-          <img class="volillustration" src="{{asset ('img/seguranca.svg')}}" alt="ilustração de limpeza">
+          <img class="volillustration" src="{{asset ('img/seguranca.svg')}}" alt="ilustração de seguranca">
           <h3 class="text-center font-weight-bold">Segurança</h3>
           <p class="text-center">Manter as florestas fora de perigo de incêndio</p>
         </div>
         <div class="m-5">
-          <img class="volillustration" src="{{asset ('img/plantacao.svg')}}" alt="ilustração de limpeza">
+          <img class="volillustration" src="{{asset ('img/plantacao.svg')}}" alt="ilustração de plantacao">
           <h3 class="text-center font-weight-bold">Plantação</h3>
           <p class="text-center">Ajudar na reconstrução das florestas e espaços verdes</p>
         </div>
         <div class="m-5">
-          <img class="volillustration" src="{{asset ('img/cuidar.svg')}}" alt="ilustração de limpeza">
+          <img class="volillustration" src="{{asset ('img/cuidar.svg')}}" alt="ilustração de montanhas">
           <h3 class="text-center font-weight-bold">Cuidar</h3>
           <p class="text-center">Preservar o património florestal</p>
         </div>
       </div>
       <div class="col-md-12 d-flex justify-content-center">
-        <a><button class="btn green-btn1">Saber mais</button></a>
+        <a><button class="btn green-btn1">Sobre nós</button></a>
       </div>
     </div>
   </div>
@@ -157,11 +155,11 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12 text-center">
-        <h1>A comunidade da associação já é uma família</h1>
+        <h1>A nossa comunidade é muita mais do que imaginas</h1>
         <h5>Para chegarmos onde queremos precisamos da tua ajuda, a comunidade é o caminho para o sucesso.
         </h5>
       </div>
-      <div class="col-md-12 d-flex justify-content-center hidden2">
+      <div class="col-md-12 d-flex justify-content-center hidden2 suguecards">
 
         <div class="m-5 sugue-box">
           <div class="d-flex">
@@ -214,7 +212,10 @@
             <a href=""><span class="more text-white">Saber mais</span></a>
         </div>
         </div>
+        <h5 class="text-center pb-4">Descobre as vantagens de te tornares membro.
+        </h5>
         <div class="col-md-12 d-flex justify-content-center">
+          
         <a class="link-light" href="{{route('tornarMembro')}}"><button class="btn green-btn1">Tornar membro</button></a>
         <a><button class="btn hero-btn2">Sugestões</button></a>
       </div>
