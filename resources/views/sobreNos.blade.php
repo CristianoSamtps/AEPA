811b16cc800979308bcc7383e8ef4602ae7af816
CristianoSantos <cristiano_leiria_epl@hotmail.com>
2023-11-22 07:10:18 +0000

    SobreNOS - UPDATE

--- resources/views/sobreNos.blade.php
+++ resources/views/sobreNos.blade.php
@@ -47,9 +47,11 @@
       </div>
     </div>
   </section>
-
   <!-- End #hero -->
 
+  <div class="backgroud">
+    <img src="{{asset ('img/greyvector.svg')}}" alt="background-effects">
+  </div>
   <!-- Proteção ambietal-->
 
   <section class="container" id="protecaoAmbiental">
@@ -67,18 +69,17 @@
       </div>
       <div class="col-md-6 " id="protecaoAmbientalinfo">
         <span>Atualizado a 12-11-2023</span>
-        <h1>História e Missão</h1>
-        <h4>Juntos pela Natureza, a AEPA trilha caminhos de preservação e sustentabilidade. Somos uma comunidade comprometida com a projeção do ambiente.
+        <h1>Proteção Ambiental</h1>
+        <h4>Os projetos da associação estão destinados aos mais necessitados, as maiores tragédias de deflorestação é o ponto mais importante para nós. Temos objetivos definidos para que estes acontecimentos sejam cada vez menos frequentes.
         </h4>
       </div>
     </div>
   </div>
 </section>
-
 <!-- End Proteção ambietal-->
 
-<!-- Impacto social -->
 
+<!-- Impacto social -->
 <section class="container" id="impactosocial">
   <div class="row">
     <div class="col-md-6" id="impactosocialinfo">
@@ -94,15 +95,17 @@
 </section>
 
 <!-- End Impacto social -->
-
-<section class="container">
+<section class="container" id="QA">
   <div class="row">
     <div class="col-md-6">
+      <object data="{{asset ('img/QA.svg')}}" type="image/svg+xml"></object>
     </div>
     <div class="col-md-6">
-
+      <div class="QAtitle">
+        <span>Atualizado a 21-11-2023</span>
+      <h1>Q&A Preguntas Frequentes</h1>
+      </div>
       <div class="accordion" id="accordionExample">
-
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingOne">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -111,63 +114,55 @@
           </h2>
           <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              <p>Os fundos das doações são direcionados para uma variedade de iniciativas ambientais, incluindo projetos de conservação, educação ambiental e sensibilização, programas de reflorestação e a implementação de práticas sustentáveis.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Como é que posso participar num evento? 
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <p>Participar nos nossos eventos é simples! Basta estar atento ao nosso calendário de eventos, disponível no nosso site e nas redes sociais. Geralmente, as inscrições são feitas online através de formulários simples que garantem uma fácil participação.
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Os eventos são gratuitos?
+                </button>
+              </h2>
+              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <p>A maioria dos nossos eventos é gratuita para o público, como parte do nosso compromisso em tornar a participação acessível a todos. No entanto, eventos especiais ou programas específicos podem ter uma pequena taxa associada para cobrir custos operacionais. Essas informações detalhadas sobre custos, se aplicáveis, serão sempre claramente comunicadas nas descrições dos eventos e nos materiais de divulgação.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    Como me posso voluntariar?
+                  </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                  <p>Para aceder ao processo de voluntariado pode dirigir-se a página do projeto ao qual se pretende voluntariar, e ai participar no projeto.
+                    Todas a informações necessárias poderão ser encontradas na página principal do projeto.
+                    Em caso de dúvidas contacte 962145236 ou em projetosaepa@aepa.pt
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Como é que posso participar num evento? 
-            </button>
-          </h2>
-          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
+      </section>
 
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              Os eventos são gratuitos?
-            </button>
-          </h2>
-          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-              Como me posso voluntariar?
-            </button>
-          </h2>
-          <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              Para aceder ao processo de voluntariado pode dirigir-se a página do projeto ao qual se pretende voluntariar, e ai participar no projeto.
-
-              Todas a informações necessárias poderão ser encontradas na página principal do projeto.
-
-              Em caso de dúvidas contacte 962145236 ou em projetosaepa@aepa.pt
-
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </div>
-  
-</section>
+    </main><!-- End #main -->
 
 
-</main><!-- End #main -->
-
-
-@endsection
+    @endsection
