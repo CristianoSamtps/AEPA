edadc4664fb255a66b3d15e451fcb09888badaee
Gonçalo Oliveira <2221008@my.ipleiria.pt>
2023-11-22 00:09:47 +0000

    Adição da página Detalhes do Projeto e melhorias relacionadas
    1->Ficheiro PageController.php
    1-Alteração dos controladores com a minha view (project_detail1 e project_detail2);
    2-Formatação do código;
    
    2->Ficheiro web.php:
    1-Alteração das rotas com a colocação da minha rota (project_detail1 e project_detail2);
    
    3->Ficheiro project_detail1.blade.php & project_detail2.blade.php & project_detail1.css & project_detail2.css & Ícones:
    1-Começo da minha página (Detalhes do Projeto) em versão desktop;
    
    4->Ficheiro projects.blade.php & projects.css:
    1-Colocação da hiperligação e seu estilo para a página detalhes do projeto;

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,25 +6,40 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
-    	return view('index');
+    public function index()
+    {
+        return view('index');
     }
-    public function topDonates(){
-    	return view('topDonates');
+    public function topDonates()
+    {
+        return view('topDonates');
     }
-    public function patrocinadores(){
-    	return view('patrocinadores');
+    public function patrocinadores()
+    {
+        return view('patrocinadores');
     }
 
-    public function projects(){
+    public function projects()
+    {
         return view('projects');
     }
 
-    public function tornarMembro(){
+    public function project_detail1()
+    {
+        return view('project_detail1');
+    }
+
+    public function project_detail2()
+    {
+        return view('project_detail2');
+    }
+
+    public function tornarMembro()
+    {
         return view('tornarMembro');
     }
-    public function sobreNos(){
+    public function sobreNos()
+    {
         return view('sobreNos');
     }
 }
-
