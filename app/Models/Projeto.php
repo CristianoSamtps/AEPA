480a74b7974e111aa08fd99930d85b9b37963c26
Gonçalo Oliveira <2221008@my.ipleiria.pt>
2023-12-06 18:49:07 +0000

    Models Projeto & FotografiaProjeto
    Atualização dos Models

--- app/Models/Projeto.php
+++ app/Models/Projeto.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Projeto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'subtitulo',
+        'descricao',
+        'estado',
+        'localidade',
+        'objetivos',
+        'data_final',
+        'voluntariado',
+        'partnership_id',
+    ];
+
+    public function fotografias()
+    {
+        return $this->hasMany(FotografiaProjeto::class, 'projeto_id');
+    }
+
+    public function partnership()
+    {
+        return $this->belongsTo(Partnership::class);
+    }
 }
