445d45acc773624787fa31246ed9753661434bbd
2221010Andre <2221010@my.ipleiria.pt>
2024-01-29 17:40:07 +0000

    .

--- resources/views/editperfil.blade.php
+++ resources/views/editperfil.blade.php
@@ -82,8 +82,6 @@
 
                     <a href="{{ route('editperfil', auth()->user()) }}"><button class="cancelar" style="display: none;">Cancelar</button></a>
 
-                    <a style="text-decoration: none" href="{{ route('tornarMembro') }}"><button id="membroBtn" class="membro" style="display: block;">Torne-se
-                            Membro</button></a>
 
                 </div>
 
