794064bd38afbfe850e9c09b0ae734f4005684e8
2221010Andre <2221010@my.ipleiria.pt>
2024-01-30 17:36:27 +0000

    galeria final

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -9,19 +9,6 @@
 @section('main')
 
 <main id="main">
-    <style>
-        /* Default height for small devices */
-        #intro-example {
-            height: 400px;
-        }
-
-        /* Height for devices larger than 992px */
-        @media (min-width: 992px) {
-            #intro-example {
-                height: 600px;
-            }
-        }
-    </style>
 
     <div id="intro-example" class="p-5 text-center bg-image position-relative">
         <div class="mask d-flex justify-content-center align-items-center h-100">
@@ -29,7 +16,7 @@
                 <div class="text-white">
                     <h1 class="mb-3">Galeria</h1>
                     <h5 class="mb-4">
-                        Um novo mundo connosco!
+                        Por um mundo mais alegre!
                     </h5>
                     <a href="{{ route('index') }}" class="btn btn-outline-light btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button">Página Inicial</a>
                     <a href="#destaque" class="btn btn-outline-light btn-lg m-2" href="https://mdbootstrap.com/docs/standard/" role="button">Video <i class="fa-brands fa-youtube"></i></a>
@@ -41,7 +28,7 @@
     <!-- Projetos -->
     <div class="row" id="fragma">
         @php
-        $fotosEmbaralhadas = $fotografias->shuffle()->take(6);
+        $fotosEmbaralhadas = $fotografias->shuffle()->take(9);
         @endphp
 
         @foreach ($fotosEmbaralhadas as $fotografia)
@@ -71,7 +58,7 @@
     <!-- Eventos -->
     <div class="row" id="fragma">
         @php
-        $fotosEmbaralhadas = $fotografiaseventos->shuffle()->take(6);
+        $fotosEmbaralhadas = $fotografiaseventos->shuffle()->take(9);
         @endphp
 
         @foreach ($fotosEmbaralhadas as $fotografiaevent)
